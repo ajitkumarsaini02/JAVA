514f7e606dbe02df16e9e56096dbb91edb7bf37c
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-04 19:06:00 +0530

    ..

---
+++ Basic/Namaste.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class Namaste {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        // Using if else statement
+        // if(button ==1){
+        //     System.out.println("Hello");
+        // }
+        // else if(button==2){
+        //     System.out.println("Namaste");
+        // }
+        // else if(button==3){
+        //     System.out.println("Bonjour");
+        // }
+        // else{
+        //     System.out.println("Invalid button");
+        // }
+
+        //Using Switch statement 
+        switch(button){
+            case 1:
+                System.out.println("Hello");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Bonjour");
+                break;
+            default:
+                System.out.println("Invalid Button");
+        }
+
+    }
+}
