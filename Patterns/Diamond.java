7c371194caa74d66095d92a5a289653c86dd1072
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-05 10:51:59 +0530

    ..

---
+++ Patterns/Diamond.java
@@ -0,0 +1,26 @@
+public class Diamond {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            int stars = 2*i-1;
+            for(int j=1;j<=stars;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            int stars = 2*i-1;
+            for(int j=1;j<=stars;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
