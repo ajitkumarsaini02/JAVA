9178a12f4bddcc380c33cb65acfe542dae1acff1
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-05 10:54:10 +0530

    ..

---
+++ Patterns/InvertedHalfPyramidNumber.java
@@ -0,0 +1,20 @@
+public class InvertedHalfPyramidNumber {
+    public static void main(String[] args) {
+        int n=7;
+        // Ist Method
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        // 2nd Method
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
