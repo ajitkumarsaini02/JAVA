c2f135d61a4e0ec3f80f04bd6f4d3c268942e4d9
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-06 22:49:32 +0530

    ..

---
+++ Strings/Declaration.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+public class Declaration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine(); // for spaces
+        System.out.println("Your name is : "+name);
+    }
+}
