42b2e0154d177df68f915ba259133604c915515f
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-06 22:46:58 +0530

    ..

---
+++ Strings/Sum.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        String arr[] = new String[size];
+        int totLength = 0;
+        for(int i=0;i<size;i++){
+            arr[i] = sc.next();
+        }
+        for(int i=0;i<size;i++){
+            totLength += arr[i].length();
+        }
+        System.out.println(totLength);
+    }
+    
+}
