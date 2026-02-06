5c4c72c60c6923f9d741d00c0158d0f5a1057a7b
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-06 22:53:15 +0530

    ..

---
+++ Sortings/InsertionSort.java
@@ -0,0 +1,9 @@
+
+public class InsertionSort {
+    int a;
+   public static void main(String[] args) {
+       InsertionSort obj = new InsertionSort();
+    System.out.println(obj.a);
+    
+   }
+}
