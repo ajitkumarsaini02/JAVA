86e1f095b9f0790aca80d2690de4befc0353fe16
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-02-06 22:45:56 +0530

    ..

---
+++ Functions/Factorial.java
@@ -0,0 +1,24 @@
+package functions;
+
+import java.util.Scanner;
+
+
+public class Factorial {
+    public static void Printfact(int n){
+        int a=1;
+        if(n<0){
+            System.out.println("Invalid Number");
+            return;
+        }
+        for(int i=n;i>=1;i--){
+            a*=i;
+        }
+        System.out.println("Factorial of "+n+" : "+a);
+   }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int n = sc.nextInt();
+        Printfact(n);
+    }
+}
