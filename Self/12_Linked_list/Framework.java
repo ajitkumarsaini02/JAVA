04b871a65546b74e5c8a95b78dc0bb46319e2cc2
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-03-06 20:40:51 +0530

    ..

---
+++ Self/12_Linked_list/Framework.java
@@ -0,0 +1,33 @@
+
+import java.util.LinkedList;
+
+public class Framework {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+
+        list.add("list"); // add last
+        System.out.println(list);
+        list.addFirst("This");
+        System.out.println(list);
+
+        System.out.println(list.size());
+
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" -> ");
+        }
+        System.out.println("null");
+
+        list.remove(3);
+        System.out.println(list);
+        
+        list.removeFirst();
+        System.out.println(list);
+
+        list.removeLast();
+        System.out.println(list);
+    }
+}
