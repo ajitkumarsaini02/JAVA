258fdb6a9d87c38e554125f2a974c9a4762955df
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2026-05-09 13:56:09 +0530

    ..

---
+++ CLG/OOP/Record.java
@@ -0,0 +1,9 @@
+record Employee(String name, int id) {
+    // You can add methods or constructors here if needed
+}
+public class Record {
+    public static void main(String[] args) {
+        Employee emp = new Employee("John Doe", 123);
+        System.out.println(emp);
+    }
+}
